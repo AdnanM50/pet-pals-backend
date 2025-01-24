ddd7effe60b921322e2c90250cdcbc5097a42a25
AdnanM50 <a-19150@mangrove.edu.bd>
2025-01-24 23:15:15 +0600

    Add user login and profile retrieval functionality; enhance user registration with logging

--- src/routes/api/user.routes.ts
+++ src/routes/api/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { findUser, sendOtp, userRegistration } from "../../controllers/user.controller";
+import { findUser, getUserProfile, sendOtp, userLogin, userRegistration } from "../../controllers/user.controller";
 // import { get } from "http";
 
 
@@ -8,8 +8,10 @@ const userRoutes = Router();
 
 
 userRoutes.post('/register', userRegistration);
+userRoutes.post('/login', userLogin);
 userRoutes.post('/send-otp', sendOtp);
 userRoutes.get('/find', findUser);
+userRoutes.get('/profile',getUserProfile); 
 userRoutes.get('/get', (req, res) => {
     res.send('Hello Adnan')
 })
