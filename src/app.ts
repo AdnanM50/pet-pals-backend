d61ea5a9a4220ce4be688e5a5e37efd8c643f045
AdnanM50 <a-19150@mangrove.edu.bd>
2025-03-22 22:17:11 +0600

    Update package-lock.json license information and enhance user routes response structure

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ app.use(
     })
 );
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); //* will allow from all cross domain
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
--- src/routes/api/user.routes.ts
+++ src/routes/api/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { findUser, getUserProfile, userLogout, sendOtp, userLogin, userRegistration } from "../../controllers/user.controller";
 import { isAdmin } from "../../middlewares/auth.middleware";
-import { log } from "console";
-import { stat } from "fs";
+// import { log } from "console";
+// import { stat } from "fs";
 // import { isUser } from "../../middlewares/auth.middleware";
 
 
@@ -19,7 +19,10 @@ userRoutes.get('/profile',getUserProfile);
 userRoutes.post('/logout', userLogout);
 userRoutes.get('/get', (req, res) => {
     res.send(
-     {status: '200', message: 'success', data: 'get user',success: true}
+     {status: '200', message: 'success', data: [
+            {name: 'John Doe', email: 'jone@gmail.com', phone: '1234567890'},
+            {name: 'Adnan Doe', email: 'adnan@gmail.com', phone: '1234567899'}
+     ],success: true}
     )
 })
 
